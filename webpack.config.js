2c7c4f48c4fe9ff6cc0b1abb2db9b0f551e5274a
Ezenwankwo <ezenwankwogabriel@gmail.com>
2019-10-03 22:19:07 +0100

    Configuring a full-stack Javascript Environment: Client Side

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './lib/components/Index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
